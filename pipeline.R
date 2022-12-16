3e24f03e1b6b54ebcf62da4196769d88db63249f
C-Heininger <c.heininger@medunigraz.at>
2022-12-16 12:06:44 +0100

    added up to step 4 (not finished)

--- pipeline.R
+++ pipeline.R
@@ -1,7 +1,6 @@
 
 # Read command line arguments through optparse package ####
 
-
 library(optparse)
 
 option_list <- list(
@@ -53,16 +52,18 @@ if(is.null(opt$threads)) {
 
 work_dir <- opt$work_dir
 
+logs_dir <- paste0(work_dir, "/analysis/logs")
+if(!file.exists(logs_dir)) { dir.create(logs_dir, recursive = TRUE) }
+
 threads <- as.integer(opt$threads)
 
 read_1 <- paste0(opt$input_file, "_R1.fastq")
 read_2 <- paste0(opt$input_file, "_R2.fastq")
 
 
-setwd(opt$work_dir)
+setwd(work_dir)
 
 # Load package for execute function or define if package is not installed ####
-
 
 if(require("execute")) {
   library(execute)
@@ -87,22 +88,137 @@ if(require("execute")) {
 cutadapt_out <- paste0(work_dir, "/data/UMIonly")
 if(!file.exists(cutadapt_out)) { dir.create(cutadapt_out, recursive = TRUE) }
 
-cutadapt_log <- paste0(work_dir, "/analysis/logs/cutadapt")
+cutadapt_log <- paste0(logs_dir, "/cutadapt")
 if(!file.exists(cutadapt_log)) { dir.create(cutadapt_log, recursive = TRUE) }
 
 cutadapt_threads <- as.integer(threads / 3)
 
 
 cutadapt_cmd <- paste0("cutadapt -g ^\"N{6}A\" -G ^\"N{6}C\" --discard-untrimmed --action=retain --cores=",
-                       cutadapt_threads, " --pair-filter=both ", read_1, " ", read_2,
-                       " -o ", cutadapt_out, "/", basename(read_1),
-                       " -p ", cutadapt_out, "/", basename(read_2),
+                       cutadapt_threads, "--pair-filter=both ", read_1, " ", read_2,
+                       " -o ", cutadapt_out, "/", basename(read_1), " -p ", cutadapt_out, "/", basename(read_2),
                        " 2>&1 ", cutadapt_log, "/", basename(opt$input_file), "_cutadapt.log")
 
-execute(cutadapt_cmd, outputfile = )
+execute(cutadapt_cmd, outputfile = paste0(cutadapt_out, "/", basename(read_1)))
 
 
-# Step 2: Processing reads with fastp and bowtie2
+# Step 2: Processing reads with fastp and bowtie2 ####
+
+processing_out <- paste0(work_dir, "/data/processed_fastq")
+if(!file.exists(processing_out)) { dir.create(processing_out, recursive = TRUE) }
+
+fastp_log <- paste0(logs_dir, "/fastp")
+if(!file.exists(fastp_log)) { dir.create(fastp_log, recursive = TRUE) }
+
+rrna_log <- paste0(logs_dir, "/rRNA")
+if(!file.exists(rrna_log)) { dir.create(rrna_log, recursive = TRUE) }
+
+processing_threads <- as.integer((threads / 3) * 2)
+
+
+processing_cmd <- paste0("(fastp -i ", cutadapt_out, "/", basename(read_1), " -I ", cutadapt_out, "/", basename(read_2),
+                         " --umi --umi_loc=per_read --umi_len=6 --trim_poly_g --trim_poly_x --adapter_sequence ", opt$adapter_r1,
+                         " --adapter_sequence_r2 ", opt$adapter_r2, " --stdout --html ", fastp_log, "/", basename(opt$input_file),
+                         "_fastp.html --json ", fastp_log, "/", basename(opt$input_file), "_fastp.json --thread ", processing_threads,
+                         " -c 2>&1 ", fastp_log, "/", basename(opt$input_file), "_fastp.log) | (bowtie2 --fast-local --un-conc ",
+                         processing_out, "/", basename(opt$input_file), ".fastq --interleaved - -x ", opt$rdna_index,
+                         " --threads ", processing_threads, " 2>&1 ", rrna_log, "/", basename(opt$input_file), "_rRNA_bowtie.log) > /dev/null")
+
+
+execute(processing_cmd, outputfile = paste0(processing_out, "/", basename(read_1)))
+
+
+# Renaming output from bowtie2 
+
+old_names <- list.files(path = processing_out)
+
+new_names <- gsub("\\.R", "\\_R", old_names)
+
+file.rename(paste0(processing_out, old_names), paste0(processing_out, new_names))
+
+
+# Step 3: Aligning reads to mm39 using STAR aligner
+
+sorted_out <- paste0(work_dir, "/data/sorted_fastq")
+if(!file.exists(sorted_out)) { dir.create(sorted_out, recursive = TRUE) }
+
+align_out <- paste0(work_dir, "/data/BAM")
+if(!file.exists(align_out)) { dir.create(align_out, recursive = TRUE) }
+
+sorted_files <- c(paste0(work_dir, "/", sorted_out, "/", opt$input_file, "_R1_sorted.fastq"),
+                  paste0(work_dir, "/", sorted_out, "/", opt$input_file, "_R2_sorted.fastq"))
+
+sort1_cmd <- paste0("fastq-sort -n -S 4G \"", processing_out, "/", basename(read_1), "\" > ", sorted_files[1])
+
+sort2_cmd <- paste0("fastq-sort -n -S 4G \"", processing_out, "/", basename(read_2), "\" > ", sorted_files[2])
+
+execute(sort1_cmd, sorted_files[1])
+
+execute(sort2_cmd, sorted_files[2])
+
+
+aligned_file <- c(paste0(work_dir, "/", align_out, "/", basename(opt$input_file), "_Aligned.out.sam"))
+
+
+align_cmd <- paste0("STAR --runThreadN ", threads, " --genomeDir ", opt&genome_index, 
+                    " --readFilesIn ", sorted_files[1], " ", sorted_files[2], " outFileNamePrefix \"", align_out, 
+                    "/", opt$input_file, "_\"")
+
+execute(align_cmd, aligned_file)
+
+
+bam_out <- paste0(align_out, "/", basename(opt$input_file), ".BAM")
+
+samtools_threads <- as.integer((threads / 3) * 2)
+
+samviewsort_cmd <- paste0("samtools view -bS \"", aligned_file[1], "\" | samtools sort -@ ",
+                          samtools_threads, " -o \"", bam_out, "\"")
+
+execute(samviewsort_cmd, bam_out)
+
+
+samindex_cmd <- paste0("samtools index \"", bam_out, "\"")
+
+execute(samindex_cmd, outputfile = paste0(bam_out, ".bai"))
+
+
+# Step 4: Deduplicating BAM files based on UMIs ####
+
+
+deduped_out <- paste0(work_dir, "/data/deduped_BAM")
+if(!file.exists(deduped_out)) { dir.create(deduped_out, recursive = TRUE) }
+
+deduped_log <- paste0(logs_dir, "/dedup")
+if(!file.exists(deduped_log)) { dir.create(deduped_log, recursive = TRUE) }
+
+
+deduped_file <- paste0(deduped_out, "/", basename(opt$input_file), "_deduped.BAM")
+
+
+# mkdir -p data/deduped_BAM/
+#   mkdir -p analysis/logs/dedup/
+#   
+#   echo -e "Deduplicating BAM files based on UMIs and indexing deduped BAM files... \n"
+# date
+# 
+# for FILE in data/BAM/.*BAM
+# do
+# echo -e "Deduplicating ${FILE} and indexing... \n"
+# date
+# (umi_tools dedup \
+#   -I "$FILE" \
+#   --paired \
+#   --umi-separator=":" \
+#   --output-stats="analysis/logs/dedup/$(basename ${FILE%.BAM})" \
+#   -S "data/deduped_BAM/$(basename ${FILE%.BAM}_deduped.BAM)" \
+# )2>&1 "analysis/logs/dedup/$(basename ${FILE%.BAM})_deduped_${TIMESTAMP}.log" &&
+#   samtools index "data/deduped_BAM/$(basename ${FILE%.BAM}_deduped.BAM)"
+# done
+
+
+
+
+
 
 
 
